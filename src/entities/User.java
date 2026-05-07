abd665b14a1ebf411a6b6f7f28ee1586950952fe
italodesa <italodesa0101@gmail.com>
2026-05-07 10:44:48 -0300

    feat(User): add createUser static method

--- src/entities/User.java
+++ src/entities/User.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Scanner;
+
 public class User {
     String name;
     String password;
@@ -28,5 +30,26 @@ public class User {
 
     public String getUserType() {
         return userType;
+    }
+
+    public static void createUser(Scanner sc) {
+        String userType = "";
+        System.out.println("Digite o nome do usuario:");
+        String name = sc.nextLine();
+        System.out.println("Digite a senha do seu usuario:");
+        String password = sc.nextLine();
+        System.out.println("Digite o tipo de usuario:");
+        System.out.println("[1] Cliente\n[2] Vendedor");
+        int choice = sc.nextInt();
+
+        if (choice == 1) {
+            userType = "seller";
+        } else if (choice == 2) {
+            userType = "client";
+        } else {
+            System.out.println("Digite um tipo valido");
+            return;
+        }
+        User user = new User(name, password, userType);
     }
 }

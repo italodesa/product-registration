038b12a9808bce69bf784865c8c989e4fa8b86f7
italodesa <italodesa0101@gmail.com>
2026-05-06 22:45:30 -0300

    refactor: add option create product in mainMenu method

--- src/entities/Menu.java
+++ src/entities/Menu.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Menu {
     public static void mainMenu(Scanner sc){
         System.out.println("=====" + "WEB MARKET" + "=====" );
-        System.out.println("[1] View products\n[2] Exit");
+        System.out.println("[1] View products\n[2] Create product\n[3] Exit");
         byte choice = sc.nextByte();
     }
 }

5641a96c892044f4c4b252f40d7478e87564b5bc
italodesa <italodesa0101@gmail.com>
2026-05-06 22:20:40 -0300

    feat: add Product class constructor

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -4,4 +4,10 @@ public class Product {
     String name;
     double price;
     int quantity;
+
+    public Product(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }

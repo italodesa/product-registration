2b529583634e2ee649c525a513386844896e35cd
italodesa <italodesa0101@gmail.com>
2026-05-06 22:34:38 -0300

    feat: add allProducts ArrayList in main method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import entities.Product;
+
+import java.util.ArrayList;
+
 public class Program {
     public static void main(String[] args){
+        ArrayList<Product> allProducts = new ArrayList<Product>();
 
     }
 }

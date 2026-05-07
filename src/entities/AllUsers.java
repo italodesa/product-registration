3c10290ee123afcc183c8702b7aa664cfff72d54
italodesa <italodesa0101@gmail.com>
2026-05-07 11:10:18 -0300

    feat(AllUsers): add addUser method

--- src/entities/AllUsers.java
+++ src/entities/AllUsers.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class AllUsers {
     private ArrayList<User> users = new ArrayList<User>();
 
+    public void addUser(User user){
+        users.add(user);
+    }
+
     public User getUser(String name, String password) {
         for(User user: this.users){
             if (user.getName().equals(name) && user.getPassword().equals(password)){

eb7821a8d721a36508bf984e7285835561798722
italodesa <italodesa0101@gmail.com>
2026-05-06 23:50:58 -0300

    feat: add logic in mainMenu

--- src/entities/Menu.java
+++ src/entities/Menu.java
@@ -3,9 +3,23 @@ package entities;
 import java.util.Scanner;
 
 public class Menu {
-    public static void mainMenu(Scanner sc){
-        System.out.println("=====" + "WEB MARKET" + "=====" );
-        System.out.println("[1] View products\n[2] Create product\n[3] Exit");
-        byte choice = sc.nextByte();
+
+    public static void mainMenu(Scanner sc, AllProducts allProducts) {
+        while (true) {
+            System.out.println("=====" + "WEB MARKET" + "=====" );
+            System.out.println("[1] View products\n[2] Create product\n[3] Exit");
+            byte choice = sc.nextByte();
+            sc.nextLine();
+            switch(choice){
+                case 1:
+                    allProducts.listProducts();
+                    break;
+                case 2:
+                    Product.createProduct(sc, allProducts);
+                    break;
+                case 3:
+                    System.exit(0);
+            }
+        }
     }
 }

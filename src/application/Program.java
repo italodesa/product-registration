cba185628115904f57e1b1452a205fb7e3541db0
italodesa <italodesa0101@gmail.com>
2026-05-07 10:58:00 -0300

    feat(Program): add users ArrayList in main method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import entities.AllProducts;
+import entities.AllUsers;
 import entities.Menu;
 
 import java.util.Scanner;
@@ -8,6 +9,7 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args){
         AllProducts products =  new AllProducts();
+        AllUsers users = new AllUsers();
         Scanner sc = new Scanner(System.in);
         Menu.mainMenu(sc,products);
 
